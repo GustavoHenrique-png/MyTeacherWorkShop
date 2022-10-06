631d89b23046dc18d971802d9c9dcc12dfa644d1
Gustavo Henrique <ymooncake@gmail.com>
2022-10-06 15:30:04 -0300

    styled components(06/10/2022)

---
+++ frontend/src/components/Header/Header.style.tsx
@@ -0,0 +1,7 @@
+import { styled } from "@mui/system";
+
+export const HeaderContainer = styled('header')`
+    color: red;
+
+`
+//importante a instalaçao do muimaterial
---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import { HeaderContainer } from "./Header.style"
+
+const Header = () =>{
+    return(
+        <HeaderContainer>TreinaWeb</HeaderContainer>
+    )
+}
+
+export default Header
